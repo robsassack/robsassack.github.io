c0a888582d625aa215e715e70dbab038dc36a855
Rob Sassack <rsassack25@gmail.com>
2022-12-12 15:21:31 -0500

    Updated links

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -4,10 +4,6 @@ function Socials() {
       <a href='./Resume_Robert-Sassack-1122_v2.pdf'>Resume</a>
       <a href='https://github.com/robsassack'>GitHub</a>
       <a href='https://linkedin.com/in/robsassack'>LinkedIn</a>
-      <a href='https://www.youtube.com/channel/UCR6ToeNGImjmu-IGDBgDiOg'>
-        YouTube
-      </a>
-      <a href='https://twitter.com/robsassack'>Twitter</a>
     </div>
   );
 }
